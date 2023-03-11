4a0abc45098bfb851e26cd7508f71167762402a0
Grenish rai <ariestheracer@gmail.com>
2023-03-11 16:01:44 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Headers>
+        <Nav />
+      </Headers>
+
+      
+    </div>
+  );
+}
+
+export default App;
