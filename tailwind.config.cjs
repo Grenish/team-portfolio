63518e253ca7cc753a14e4fcc49c1651070b889a
Grenish rai <ariestheracer@gmail.com>
2023-03-14 03:47:59 +0530

    tailwindcss config files

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        palette1: {
+          1: '#e0ddca',
+          2: '#f6f8e2',
+          3: '#9ccd62',
+          4: '#3c3d42',
+        }
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+  },
+  plugins: [],
+}
